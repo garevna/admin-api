7cf2b325a601f6fb1a7a9cfe7b24008b5534e99d
Maksim Black <mbkkong@gmail.com>
2020-01-17 17:36:14 +0200

    Change basecontroll

--- controllers/BaseController.js
+++ controllers/BaseController.js
@@ -1,6 +1,11 @@
 const fs = require('fs-extra')
+const http = require('http2')
 
 class BaseController {
+	constructor () {
+		this.http = http
+	}
+
 	/** Method for reading json from file */
 	async _readFileFromStorage (file) {
 		return JSON.parse(await fs.readFile(`${__dirname}/../storage/${file}`))
