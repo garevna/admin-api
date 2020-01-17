fd32475033ecb79c98c1482b43f47141b875747e
Maksim Black <mbkkong@gmail.com>
2020-01-17 17:36:29 +0200

    Resolve

--- controllers/BlogController.js
+++ controllers/BlogController.js
@@ -4,9 +4,11 @@ class BlogController extends BaseController {
 	async news (req, res) {
 		return res.json(await this._readJSONFromStorage('blog/news.json'))
 	}
+
 	async content (req, res) {
 		return res.json(await this._readJSONFromStorage('blog/content.json'))
 	}
+
 	async file (req, res) {
 		return res.send(await this._readTextFromStorage(`blog/content/${req.params.file}`))
 	}
