d5c4a3ff18d7bda3a2c2d14e4563ccaf63154d19
Irina Fylyppova <irina.h.fylyppova@gmail.com>
2020-01-19 22:52:42 +0200

    linter errors fix

--- routes/mail.js
+++ routes/mail.js
@@ -12,20 +12,20 @@ router.post('/', (req, res) => {
     email: req.body.email,
     username: req.body.username || req.body.name || 'guest',
     subject: req.body.subject || 'DGTek',
-		html: req.body.html || req.body.text || 'Tanks for your message'
+    html: req.body.html || req.body.text || 'Tanks for your message'
   }
 
-	EmailController.sendEmail(params, res)
+  EmailController.sendEmail(params, res)
 
-	params.email = 'info@dgtek.net'
-	params.subject = 'Contact info'
+  params.email = 'info@dgtek.net'
+  params.subject = 'Contact info'
   params.html = `<h3>user: ${params.username}</h3>
-	<p>phone: ${req.body.phone}</p>
-	<p>email: ${req.body.email}</p>
-	<h4>Message:</h4>
-	<p>${req.body.message}</p>
-	`
-	EmailController.sendEmail(params, res)
+    <p>phone: ${req.body.phone}</p>
+    <p>email: ${req.body.email}</p>
+    <h4>Message:</h4>
+    <p>${req.body.message}</p>
+  `
+  EmailController.sendEmail(params, res)
 })
 
 module.exports = router
