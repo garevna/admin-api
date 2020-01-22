081af69300050a6bc5a247ac5abb4e38985c6f76
Irina Fylyppova <irina.h.fylyppova@gmail.com>
2020-01-22 18:31:31 +0200

    tmp

--- controllers/EmailController.js
+++ controllers/EmailController.js
@@ -1,4 +1,5 @@
 const nodemailer = require('nodemailer')
+const { validationResult } = require('express-validator')
 const BaseController = require('./BaseController')
 
 class EmailController extends BaseController {
@@ -17,41 +18,17 @@ class EmailController extends BaseController {
     })
   }
 
-  create (req, res) {
-    if (!req.body.email || !this.validMail(req.body.email)) { return res.json({ error: 'Invalid email' }) }
-
-    const params = {
-      email: req.body.email,
-      username: req.body.username || req.body.name || 'guest',
-      subject: req.body.subject || 'DGTek',
-      html: req.body.html || req.body.text || 'Tanks for your message'
-    }
-
-    this.sendEmail(params, res)
-
-    params.email = 'info@dgtek.net'
-    params.subject = 'Contact info'
-    params.html = `<h3>user: ${params.username}</h3>
-        <p>phone: ${req.body.phone}</p>
-        <p>email: ${req.body.email}</p>
-        <h4>Message:</h4>
-        <p>${req.body.message}</p>
-      `
-    return this.sendEmail(params, res)
-  }
-
   validMail (email) {
     return Boolean(email.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/))
   }
 
-  async sendEmail (params, res) {
+  sendEmail (params) {
     this.transport.sendMail({
       from: process.env.GMAIL,
       to: params.email,
       subject: params.subject,
       html: params.html
     })
-    return res.json(params)
   }
 }
 
--- routes/mail.js
+++ routes/mail.js
@@ -4,6 +4,31 @@ const router = require('express').Router()
 const { EmailController } = require('../controllers')
 
 /** Routes */
-router.post('/', (req, res) => EmailController.create(req, res))
+router.post('/', (req, res) => {
+  if (!req.body.email || !EmailController.validMail(req.body.email)) {
+    return res.json({ error: 'Invalid email' })
+  }
+
+  const params = {
+    email: req.body.email,
+    username: req.body.username || req.body.name || 'guest',
+    subject: req.body.subject || 'DGTek',
+    html: req.body.html || req.body.text
+  }
+
+  EmailController.sendEmail(params, res)
+
+  params.email = 'info@dgtek.net'
+  params.subject = 'Contact info'
+  params.html = `<h3>user: ${params.username}</h3>
+    <p>phone: ${req.body.phone || ''}</p>
+    <p>email: ${req.body.email}</p>
+    <h4>Message:</h4>
+    <p>${req.body.message || ''}</p>
+  `
+  EmailController.sendEmail(params, res)
+
+  return res.sendStatus(200)
+})
 
 module.exports = router
